12b2d92fe0506d883b1659f54a049c5f62f17839
JamesT42 <j.tuvix@gmx.de>
2014-04-20 10:10:56 +0200

    Integrated line numbering, checksum and resend reprap-style
    Checksum code is from the Reprap wiki, if the checksum doesn't match it
    requests a resend via "rs [linenumber]"
    New command structure:
    N[linenumber] [commands like before]*checksum

--- SphereBot Arduino/SphereBot/SphereBot.ino
+++ SphereBot Arduino/SphereBot/SphereBot.ino
@@ -53,7 +53,7 @@ volatile long intervals_remaining=0;
 volatile boolean isRunning=false;
 
 // comm variables
-const int MAX_CMD_SIZE = 256;
+const int MAX_CMD_SIZE = 64;
 char buffer[MAX_CMD_SIZE]; // buffer for serial commands
 char serial_char; // value for each byte read in from serial comms
 int serial_count = 0; // current length of command
@@ -257,178 +257,212 @@ void check_for_version_controll(char command)
 
 void process_commands(char command[], int command_length) // deals with standardized input from serial connection
 {
-  if(command_length == 1)
+  
+  double nVal;
+  boolean hasNVal = getValue('N', command, &nVal);
+  //if(hasNVal) {Serial.println("linenumber detected");}; 
+  
+  double getcs;
+  boolean hasCS = getValue('*', command, &getcs);
+  //if(hasCS) {Serial.println("checksum detected");};
+  
+  // checksum code from reprap wiki
+  int cs = 0;
+  int j = 0;
+  for(j = 0; command[j] != '*' && command[j] != NULL; j++)
+  cs = cs ^ command[j];
+  cs &= 0xff;  // Defensive programming...
+  
+  if(cs == (int)getcs || hasCS == false) //continue if checksum matches or none detected
   {
-    check_for_version_controll(command[0]);
-  }
-  if (command_length>0 && command[0] == 'G') // G code
-  {
-    //Serial.print("process G: \n");
-    int codenum = (int)strtod(&command[1], NULL);
-    
-    double tempX = xAxisStepper.getCurrentPosition();
-    double tempY = rotationStepper.getCurrentPosition();
-    
-    double xVal;
-    boolean hasXVal = getValue('X', command, &xVal);
-    if(hasXVal) xVal*=zoom*1.71/2;                              //this factor is for correction to meet the unicorn coordinates    
-    double yVal;
-    boolean hasYVal = getValue('Y', command, &yVal);
-    if(hasYVal) yVal*=zoom;
-    double iVal;
-    boolean hasIVal = getValue('I', command, &iVal);
-    if(hasIVal) iVal*=zoom;
-    double jVal;
-    boolean hasJVal = getValue('J', command, &jVal);
-    if(hasJVal) jVal*=zoom;
-    double rVal;
-    boolean hasRVal = getValue('R', command, &rVal);
-    if(hasRVal) rVal*=zoom;
-    double pVal;
-    boolean hasPVal = getValue('P', command, &pVal);
-    
-    getValue('F', command, &feedrate);
-
-    xVal+=currentOffsetX;
-    yVal+=currentOffsetY;
-    
-    if(absoluteMode)
+    //Serial.println("checksum match");
+    j=0;
+    while (j < MAX_CMD_SIZE ) 
     {
-      if(hasXVal)
-        tempX=xVal;
-      if(hasYVal)
-        tempY=yVal;
-    }
-    else
-    {
-      if(hasXVal)
-        tempX+=xVal;
-      if(hasYVal)
-        tempY+=yVal;
-    }
+      if ((command[j] == 'G') || command[j] == 'M') {break;}
+      j++;
+    }   
     
-    switch(codenum)
+    if(command_length == 1)
     {
-      case 0: // G0, Rapid positioning
-        xAxisStepper.setTargetPosition(tempX);
-        rotationStepper.setTargetPosition(tempY);
-        commitSteppers(maxFeedrate);
-        break;
-      case 1: // G1, linear interpolation at specified speed
-        xAxisStepper.setTargetPosition(tempX);
-        rotationStepper.setTargetPosition(tempY);
-        commitSteppers(feedrate);
-        break;
-      case 2: // G2, Clockwise arc
-      case 3: // G3, Counterclockwise arc
-        if(hasIVal && hasJVal)
-        {
-            double centerX=xAxisStepper.getCurrentPosition()+iVal;
-            double centerY=rotationStepper.getCurrentPosition()+jVal;
-            drawArc(centerX, centerY, tempX, tempY, (codenum==2));
-        }
-        else if(hasRVal)
-        {
-          //drawRadius(tempX, tempY, rVal, (codenum==2));
-        }
-        break;
-      case 4: // G4, Delay P ms
-        if(hasPVal)
-        {
-           unsigned long endDelay = millis()+ (unsigned long)pVal;
-           while(millis()<endDelay)
-           {
-              delay(1);
-              if(servoEnabled)
-                SoftwareServo::refresh();
-           }
-        }
-        break;
-      case 21: // G21 metric 
-        break;
-      case 90: // G90, Absolute Positioning
-        absoluteMode = true;
-        break;
-      case 91: // G91, Incremental Positioning
-        absoluteMode = false;
-        break;
-      case 92: // G92 homing
-        break;
+      check_for_version_controll(command[0]);
     }
-  }  
-  else if (command_length>0 && command[0] == 'M') // M code
-  {
-    //Serial.print("proces M:\n");
-    double value;
-    int codenum = (int)strtod(&command[1], NULL);
-    switch(codenum)
-    {   
-      case 18: // Disable Drives
-        xAxisStepper.resetStepper();
-        rotationStepper.resetStepper();
-        break;
-
-      case 300: // Servo Position
-        if(getValue('S', command, &value))
-        {
-          servoEnabled=true;
-          if(value<0.)
-            value=0.;
-          else if(value>180.)
+    if (command_length>0 && command[j] == 'G') // G code
+    {
+      //Serial.print("process G: \n");
+      int codenum = (int)strtod(&command[j+1], NULL);
+      
+      double tempX = xAxisStepper.getCurrentPosition();
+      double tempY = rotationStepper.getCurrentPosition();
+      
+      double xVal;
+      boolean hasXVal = getValue('X', command, &xVal);
+      if(hasXVal) xVal*=zoom*1.71/2;                              //this factor is for correction to meet the unicorn coordinates    
+      double yVal;
+      boolean hasYVal = getValue('Y', command, &yVal);
+      if(hasYVal) yVal*=zoom;
+      double iVal;
+      boolean hasIVal = getValue('I', command, &iVal);
+      if(hasIVal) iVal*=zoom;
+      double jVal;
+      boolean hasJVal = getValue('J', command, &jVal);
+      if(hasJVal) jVal*=zoom;
+      double rVal;
+      boolean hasRVal = getValue('R', command, &rVal);
+      if(hasRVal) rVal*=zoom;
+      double pVal;
+      boolean hasPVal = getValue('P', command, &pVal);
+      
+      getValue('F', command, &feedrate);
+  
+      xVal+=currentOffsetX;
+      yVal+=currentOffsetY;
+      
+      if(absoluteMode)
+      {
+        if(hasXVal)
+          tempX=xVal;
+        if(hasYVal)
+          tempY=yVal;
+      }
+      else
+      {
+        if(hasXVal)
+          tempX+=xVal;
+        if(hasYVal)
+          tempY+=yVal;
+      }
+      
+      switch(codenum)
+      {
+        case 0: // G0, Rapid positioning
+          xAxisStepper.setTargetPosition(tempX);
+          rotationStepper.setTargetPosition(tempY);
+          commitSteppers(maxFeedrate);
+          break;
+        case 1: // G1, linear interpolation at specified speed
+          xAxisStepper.setTargetPosition(tempX);
+          rotationStepper.setTargetPosition(tempY);
+          commitSteppers(feedrate);
+          break;
+        case 2: // G2, Clockwise arc
+        case 3: // G3, Counterclockwise arc
+          if(hasIVal && hasJVal)
           {
-            value=DEFAULT_PEN_UP_POSITION;
-            servo.write((int)value);
-            for(int i=0;i<100;i++)
-            {
-                SoftwareServo::refresh();
-                delay(2);
-            }           
-            servoEnabled=false;
+              double centerX=xAxisStepper.getCurrentPosition()+iVal;
+              double centerY=rotationStepper.getCurrentPosition()+jVal;
+              drawArc(centerX, centerY, tempX, tempY, (codenum==2));
           }
-          servo.write((int)value);
-        }
-        break;
-        
-      case 400: // Propretary: Reset X-Axis-Stepper settings to new object diameter
-        if(getValue('S', command, &value))
-        {
-          xAxisStepper.resetSteppersForObjectDiameter(value);
-          xAxisStepper.setTargetPosition(0.);
-          commitSteppers(maxFeedrate);
-          delay(2000);
-          xAxisStepper.enableStepper(false);
-        }
-        break;
-        
-      case 401: // Propretary: Reset Y-Axis-Stepper settings to new object diameter
-        if(getValue('S', command, &value))
-        {
-          rotationStepper.resetSteppersForObjectDiameter(value);
-          rotationStepper.setTargetPosition(0.);
-          commitSteppers(maxFeedrate);
-          delay(2000);
-          rotationStepper.enableStepper(false);
-        }
-        break;
-        
-     case 402: // Propretary: Reset Y-Axis-Stepper settings to new object diameter
-        if(getValue('S', command, &value))
-        {
-            zoom = value/100;
-        }
-        break;
-     default:
-     break;
+          else if(hasRVal)
+          {
+            //drawRadius(tempX, tempY, rVal, (codenum==2));
+          }
+          break;
+        case 4: // G4, Delay P ms
+          if(hasPVal)
+          {
+             unsigned long endDelay = millis()+ (unsigned long)pVal;
+             while(millis()<endDelay)
+             {
+                delay(1);
+                if(servoEnabled)
+                  SoftwareServo::refresh();
+             }
+          }
+          break;
+        case 21: // G21 metric 
+          break;
+        case 90: // G90, Absolute Positioning
+          absoluteMode = true;
+          break;
+        case 91: // G91, Incremental Positioning
+          absoluteMode = false;
+          break;
+        case 92: // G92 homing
+          break;
+      }
+    }  
+    else if (command_length>0 && command[j] == 'M') // M code
+    {
+      //Serial.print("proces M:\n");
+      double value;
+      int codenum = (int)strtod(&command[j+1], NULL);
+      switch(codenum)
+      {   
+        case 18: // Disable Drives
+          xAxisStepper.resetStepper();
+          rotationStepper.resetStepper();
+          break;
+  
+        case 300: // Servo Position
+          if(getValue('S', command, &value))
+          {
+            servoEnabled=true;
+            if(value<0.)
+              value=0.;
+            else if(value>180.)
+            {
+              value=DEFAULT_PEN_UP_POSITION;
+              servo.write((int)value);
+              for(int i=0;i<100;i++)
+              {
+                  SoftwareServo::refresh();
+                  delay(2);
+              }           
+              servoEnabled=false;
+            }
+            servo.write((int)value);
+          }
+          break;
+          
+        case 400: // Propretary: Reset X-Axis-Stepper settings to new object diameter
+          if(getValue('S', command, &value))
+          {
+            xAxisStepper.resetSteppersForObjectDiameter(value);
+            xAxisStepper.setTargetPosition(0.);
+            commitSteppers(maxFeedrate);
+            delay(2000);
+            xAxisStepper.enableStepper(false);
+          }
+          break;
+          
+        case 401: // Propretary: Reset Y-Axis-Stepper settings to new object diameter
+          if(getValue('S', command, &value))
+          {
+            rotationStepper.resetSteppersForObjectDiameter(value);
+            rotationStepper.setTargetPosition(0.);
+            commitSteppers(maxFeedrate);
+            delay(2000);
+            rotationStepper.enableStepper(false);
+          }
+          break;
+          
+       case 402: // Propretary: Reset Y-Axis-Stepper settings to new object diameter
+          if(getValue('S', command, &value))
+          {
+              zoom = value/100;
+          }
+          break;
+       default:
+       break;
+  
+      }
+    }  
 
-    }
-  }  
-
-//done processing commands
-//if (Serial.available() <= 0) {
-Serial.print("ok:");
-//Serial.println(command);
-Serial.print("\n");
+  //done processing commands
+  //if (Serial.available() <= 0) {
+  Serial.print("ok:");
+  //Serial.println(command);
+  Serial.print("\n");
   //}
+  
+  }
+  else 
+  {
+    Serial.print("rs ");
+    Serial.print((int)nVal);
+    Serial.print("\n");
+  }
 }
 
 /* This code was ported from the Makerbot/ReplicatorG java sources */
