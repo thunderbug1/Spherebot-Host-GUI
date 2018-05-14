38ef1ea963f9475735ff87f44c75d3cb2f9aa8dc
Blokkendoos <jvo@chaosgeordend.nl>
2018-05-15 01:17:52 +0200

    Configurate for Arduino Uno shield

--- SphereBot Arduino/SphereBot/config.h
+++ SphereBot Arduino/SphereBot/config.h
@@ -1,16 +1,16 @@
 //#define BAUDRATE 9600
-#define BAUDRATE 57600
-//#define BAUDRATE 115200
+//#define BAUDRATE 57600
+#define BAUDRATE 115200
 //#define BAUDRATE 256000
 
 /*
  * PINS
  */
 
-// Y-Axis
-#define YAXIS_DIR_PIN 14
-#define YAXIS_STEP_PIN 15
-#define YAXIS_ENABLE_PIN 21
+// Y-Axis (Pen)
+#define YAXIS_DIR_PIN    13
+#define YAXIS_STEP_PIN   12
+#define YAXIS_ENABLE_PIN 11
 #define YAXIS_RST_PIN -1
 #define YAXIS_SLP_PIN -1
 #define YAXIS_MS1_PIN -1
@@ -25,10 +25,10 @@
 #define YAXIS_STEPS_PER_FULL_ROTATION 200.0
 #define YAXIS_MICROSTEPPING 16
 
-//X-Axis
-#define XAXIS_DIR_PIN 10
-#define XAXIS_STEP_PIN 8
-#define XAXIS_ENABLE_PIN 2
+// X-Axis (Egg)
+#define XAXIS_DIR_PIN    7
+#define XAXIS_STEP_PIN   6
+#define XAXIS_ENABLE_PIN 5
 #define XAXIS_RST_PIN -1
 #define XAXIS_SLP_PIN -1
 #define XAXIS_MS1_PIN -1
@@ -41,20 +41,24 @@
 #define XAXIS_MIN_STEPCOUNT 0    // Travel limits
 #define XAXIS_MAX_STEPCOUNT 0
 #define XAXIS_STEPS_PER_FULL_ROTATION 200.0
-#define XAXIS_MICROSTEPPING 2
+#define XAXIS_MICROSTEPPING 16
 
-#define SERVO_PIN_1 13
+#define SERVO_PIN_1 10
+
+// Optional output
+#define ALT_PIN 2
 
 /*
  * Other Configuration
  */
 
-#define DEFAULT_PEN_UP_POSITION 35
+#define DEFAULT_PEN_UP_POSITION 25
 
-#define DEFAULT_ZOOM_FACTOR 0.1808 // With a Zoom-Faktor of .65, I can print gcode for Makerbot Unicorn without changes. 
-                               // The zoom factor can be also manipulated by the propretiary code M402
-#define X_SCALING_FACTOR    1.65/2    //this factor is for correction to meet the unicorn coordinates 
+#define DEFAULT_ZOOM_FACTOR 1 //0.1808 // With a Zoom-Faktor of .65, I can print gcode for Makerbot Unicorn without changes. 
+                                       // The zoom factor can be also manipulated by the propretiary code M402
+#define X_SCALING_FACTOR    1 //1.65/2 //this factor is for correction to meet the unicorn coordinates 
 #define Y_SCALING_FACTOR    1
+
 /*
  * Microstepping Information
  */     
